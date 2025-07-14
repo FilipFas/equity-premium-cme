284412e22baa24735a8a8c4a12777438c366e221
FilipFas <153379709+FilipFas@users.noreply.github.com>
2025-07-14 15:17:09 +0200

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# src/__init__.py
+
+# Import common classes/functions for easy access
+from .cleaning import DataCleaner, MultiFrequencyCleaner, RollingWindowSplitter
+from .cme import ConditionalMeanEmbedding
